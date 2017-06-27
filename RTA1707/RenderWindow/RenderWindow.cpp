cd6afa1de357587627c42eb2bfa6997062eb567f
Antonio Perez <avperez@fullsail.edu>
2017-06-27 10:00:10 -0400

    modified RenderWindow.cpp

--- RTA1707/RenderWindow/RenderWindow.cpp
+++ RTA1707/RenderWindow/RenderWindow.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "RenderWindow.h"
+#include <iostream>
 
 #define MAX_LOADSTRING 100
 
@@ -10,6 +11,7 @@
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[ MAX_LOADSTRING ];                  // The title bar text
 WCHAR szWindowClass[ MAX_LOADSTRING ];            // the main window class name
+HWND hWnd;
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass( HINSTANCE hInstance );
@@ -24,7 +26,6 @@ int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
 {
 	UNREFERENCED_PARAMETER( hPrevInstance );
 	UNREFERENCED_PARAMETER( lpCmdLine );
-
 	// TODO: Place code here.
 
 	// Initialize global strings
@@ -32,26 +33,36 @@ int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
 	LoadStringW( hInstance, IDC_RENDERWINDOW, szWindowClass, MAX_LOADSTRING );
 	MyRegisterClass( hInstance );
 
-	// Perform application initialization:
 	if ( !InitInstance( hInstance, nCmdShow ) )
 	{
 		return FALSE;
 	}
-
+#ifndef NDEBUG
+	AllocConsole();
+	FILE* new_std_in_out;
+	freopen_s( &new_std_in_out, "CONOUT$", "w", stdout );
+	freopen_s( &new_std_in_out, "CONIN$", "r", stdin );
+	std::cout << "Hello world!\n";
+#endif
 	HACCEL hAccelTable = LoadAccelerators( hInstance, MAKEINTRESOURCE( IDC_RENDERWINDOW ) );
-
 	MSG msg;
+	ZeroMemory( &msg, sizeof( msg ) );
 
-	// Main message loop:
-	while ( GetMessage( &msg, nullptr, 0, 0 ) )
+	while ( WM_QUIT != msg.message )
 	{
-		if ( !TranslateAccelerator( msg.hwnd, hAccelTable, &msg ) )
+		if ( PeekMessage( &msg, nullptr, 0, 0, PM_REMOVE ) )
 		{
-			TranslateMessage( &msg );
-			DispatchMessage( &msg );
+			if ( !TranslateAccelerator( msg.hwnd, hAccelTable, &msg ) )
+			{
+				TranslateMessage( &msg );
+				DispatchMessage( &msg );
+			}
 		}
 	}
 
+#ifndef NDEBUG
+	FreeConsole();
+#endif
 	return ( int )msg.wParam;
 }
 
@@ -76,7 +87,7 @@ ATOM MyRegisterClass( HINSTANCE hInstance )
 	wcex.hIcon = LoadIcon( hInstance, MAKEINTRESOURCE( IDI_RENDERWINDOW ) );
 	wcex.hCursor = LoadCursor( nullptr, IDC_ARROW );
 	wcex.hbrBackground = ( HBRUSH )( COLOR_WINDOW + 1 );
-	wcex.lpszMenuName = MAKEINTRESOURCEW( IDC_RENDERWINDOW );
+	wcex.lpszMenuName = nullptr;
 	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = LoadIcon( wcex.hInstance, MAKEINTRESOURCE( IDI_SMALL ) );
 
@@ -97,8 +108,8 @@ BOOL InitInstance( HINSTANCE hInstance, int nCmdShow )
 {
 	hInst = hInstance; // Store instance handle in our global variable
 
-	HWND hWnd = CreateWindowW( szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-							   CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr );
+	hWnd = CreateWindowW( szWindowClass, szTitle, WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX,
+						  CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr );
 
 	if ( !hWnd )
 	{
@@ -125,31 +136,6 @@ LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam 
 {
 	switch ( message )
 	{
-		case WM_COMMAND:
-		{
-			int wmId = LOWORD( wParam );
-			// Parse the menu selections:
-			switch ( wmId )
-			{
-				case IDM_ABOUT:
-					DialogBox( hInst, MAKEINTRESOURCE( IDD_ABOUTBOX ), hWnd, About );
-					break;
-				case IDM_EXIT:
-					DestroyWindow( hWnd );
-					break;
-				default:
-					return DefWindowProc( hWnd, message, wParam, lParam );
-			}
-		}
-		break;
-		case WM_PAINT:
-		{
-			PAINTSTRUCT ps;
-			HDC hdc = BeginPaint( hWnd, &ps );
-			// TODO: Add any drawing code that uses hdc here...
-			EndPaint( hWnd, &ps );
-		}
-		break;
 		case WM_DESTROY:
 			PostQuitMessage( 0 );
 			break;
@@ -157,24 +143,4 @@ LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam 
 			return DefWindowProc( hWnd, message, wParam, lParam );
 	}
 	return 0;
-}
-
-// Message handler for about box.
-INT_PTR CALLBACK About( HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam )
-{
-	UNREFERENCED_PARAMETER( lParam );
-	switch ( message )
-	{
-		case WM_INITDIALOG:
-			return ( INT_PTR )TRUE;
-
-		case WM_COMMAND:
-			if ( LOWORD( wParam ) == IDOK || LOWORD( wParam ) == IDCANCEL )
-			{
-				EndDialog( hDlg, LOWORD( wParam ) );
-				return ( INT_PTR )TRUE;
-			}
-			break;
-	}
-	return ( INT_PTR )FALSE;
 }
